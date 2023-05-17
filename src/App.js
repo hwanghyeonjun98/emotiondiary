f6becd4d24a04cb2278d0ed2274fddfa7f3c2cbb
JJun <jjun1010@kakao.com>
2023-05-17 10:06:50 +0900

    components, icon add

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
-import RouteTest from "./components/RouterTest";
+/// components
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeder";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
-        <h2>App.js</h2>
+        <MyHeader
+          headerText={"App"}
+          leftChild={<MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />}
+          rightChild={<MyButton text={"오른쪽 버튼"} onClick={() => alert("오른쪽 클릭")} />}
+        />
+        <MyButton text={"버튼"} type="negative" />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
